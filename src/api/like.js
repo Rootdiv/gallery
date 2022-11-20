1011ffe03092de738a938fe7dc50eb49507a49c8
Rootdiv <rootdiv@mail.ru>
2022-11-21 00:25:34 +0300

    fix: changing likes

--- src/api/like.js
+++ src/api/like.js
@@ -5,17 +5,8 @@ export const likeUpdate = (id, token, method) => {
   const url = new URL(`${API_URL_PHOTOS}/${id}/like`);
   if (!token) return;
 
-  const likeData = {};
-
   axios(url.href, {
     method,
     headers: { Authorization: `Bearer ${token}` },
-  })
-    .then(({ data }) => {
-      likeData['likes'] = data.photo.likes;
-      likeData['liked'] = data.photo.liked_by_user;
-    })
-    .catch(error => ({ error: error.toString() }));
-
-  return likeData;
+  }).catch(error => ({ error: error.toString() }));
 };
